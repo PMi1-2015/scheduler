912bc40874cc29c46e75b4231ed7d31e4e3516f0
Andriy <shpakar@gmail.com>
2017-09-14 22:11:46 +0300

    Add Entity Framework. Starting with Context and Models

---
+++ LNU.Scheduler.DataAccess/Migrations/201709141850512_Initial.cs
@@ -0,0 +1,41 @@
+namespace LNU.Scheduler.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Subjects",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Teacher_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Teachers", t => t.Teacher_Id)
+                .Index(t => t.Teacher_Id);
+            
+            CreateTable(
+                "dbo.Teachers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Subjects", "Teacher_Id", "dbo.Teachers");
+            DropIndex("dbo.Subjects", new[] { "Teacher_Id" });
+            DropTable("dbo.Teachers");
+            DropTable("dbo.Subjects");
+        }
+    }
+}
---
+++ LNU.Scheduler.DataAccess/SchedulerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LNU.Scheduler.Models;
+
+namespace LNU.Scheduler.DataAccess
+{
+    public class SchedulerContext : 
+        DbContext
+    {
+        public  DbSet<Subject> Subjects { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
